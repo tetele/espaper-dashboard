dd83ac0b548cbc94a54ceb4fc455c1d04d01020b
tetele <tm.sandu@gmail.com>
2025-08-25 11:20:56 +0000

    Add some accessors

--- components/espaper_dashboard/__init__.py
+++ components/espaper_dashboard/__init__.py
@@ -8,6 +8,8 @@ from esphome.const import (
     CONF_ID,
 )
 
+CONF_LABEL_COLOR = "label_color"
+
 espaper_dashboard_ns = cg.esphome_ns.namespace("espaper_dashboard")
 ESPaperDashboard = espaper_dashboard_ns.class_("ESPaperDashboard", cg.Component)
 
@@ -19,6 +21,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(CONF_DISPLAY_ID): cv.use_id(display.Display),
         cv.Optional(CONF_BACKGROUND_COLOR): cv.use_id(Color),
         cv.Optional(CONF_FOREGROUND_COLOR): cv.use_id(Color),
+        cv.Optional(CONF_LABEL_COLOR): cv.use_id(Color),
     }
 )
 
@@ -31,7 +34,12 @@ async def to_code(config):
     target_display = await cg.get_variable(config[CONF_DISPLAY_ID])
     cg.add(var.set_display(target_display))
 
-    bg_color = await cg.get_variable(config[CONF_BACKGROUND_COLOR])
-    cg.add(var.set_background_color(bg_color))
-    fg_color = await cg.get_variable(config[CONF_FOREGROUND_COLOR])
-    cg.add(var.set_foreground_color(fg_color))
+    if CONF_BACKGROUND_COLOR in config:
+        bg_color = await cg.get_variable(config[CONF_BACKGROUND_COLOR])
+        cg.add(var.set_background_color(bg_color))
+    if CONF_FOREGROUND_COLOR in config:
+        label_color = await cg.get_variable(config[CONF_FOREGROUND_COLOR])
+        cg.add(var.set_foreground_color(label_color))
+    if CONF_LABEL_COLOR in config:
+        label_color = await cg.get_variable(config[CONF_LABEL_COLOR])
+        cg.add(var.set_label_color(label_color))
--- components/espaper_dashboard/espaper_dashboard.h
+++ components/espaper_dashboard/espaper_dashboard.h
@@ -14,13 +14,19 @@ public:
     void dump_config() override;
 
     void set_display(display::Display *display) { this->display_ = display; };
+    display::Display *get_display() { return this->display_; };
     void set_background_color(Color color) { this->bg_color_ = color; };
+    Color get_background_color() { return this->bg_color_; };
     void set_foreground_color(Color color) { this->fg_color_ = color; };
+    Color get_foreground_color() { return this->fg_color_; };
+    void set_label_color(Color color) { this->label_color_ = color; };
+    Color get_label_color() { return this->label_color_; };
 
 protected:
     display::Display *display_{nullptr};
     Color bg_color_{display::COLOR_ON};
     Color fg_color_{display::COLOR_OFF};
+    Color label_color_{display::COLOR_OFF};
 };
 
 } // namespace espaper_dashboard
