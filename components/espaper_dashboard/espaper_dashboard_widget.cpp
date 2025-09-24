2db3623d0614590448feb0c4460fdaaa444c97f9
tetele <tm.sandu@gmail.com>
2025-09-24 19:26:14 +0000

    Log reason for redraw requirement

--- components/espaper_dashboard/espaper_dashboard_widget.cpp
+++ components/espaper_dashboard/espaper_dashboard_widget.cpp
@@ -32,7 +32,23 @@ int ESPaperDashboardWidget::get_priority()  {
 }
 
 bool ESPaperDashboardWidget::needs_redraw() {
-    return this->is_stale() || (this->was_drawn_ != this->should_draw());
+    int priority = this->get_priority();
+    if(this->old_priority_ != priority) {
+        ESP_LOGV(TAG, "[%s] Needs redraw: priority changed from %d to %d", this->id_.c_str(), this->old_priority_, priority);
+        return true;
+    }
+
+    if(this->was_drawn_ != this->should_draw()) {
+        ESP_LOGV(TAG, "[%s] Needs redraw: widget needs to %s", this->id_.c_str(), (this->was_drawn_ ? "disappear" : "appear"));
+        return true;
+    }
+
+    if(this->was_drawn_ && this->is_stale_) {
+        ESP_LOGV(TAG, "[%s] Needs redraw: widget data is stale", this->id_.c_str());
+        return true;
+    }
+
+    return false;
 }
 
 }
